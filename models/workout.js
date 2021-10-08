1f26768c76cd94c2ba0d42d36b5c13a10c6e4bff
ronarceo <ronaldmarceo@gmail.com>
2021-10-08 18:55:08 -0500

    created workout.js model in models folder

---
+++ models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            type: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+});
+
+const Workout = mongoose.model("workout", WorkoutSchema);
+
+module.exports = Workout;
