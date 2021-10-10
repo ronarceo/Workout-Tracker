d7470731cc192e350a10ca32c2709e270124d625
ronarceo <ronaldmarceo@gmail.com>
2021-10-10 15:43:06 -0500

    added route to update current workouts and to create a new workout to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,4 +18,24 @@ router.get("/api/workouts/range", (req, res) => {
     });
 });
 
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+        { _id: req.params.id },
+        { $push: { exercises: req.body } },
+        { new: true }
+    ).then((workout) => {
+        res.json(workout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts/", (req, res) => {
+    Workout.create(req.body).then((newWorkout) => {
+        res.json(newWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 module.exports = router;
