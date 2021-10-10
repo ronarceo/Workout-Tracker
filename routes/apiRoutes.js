cb8f117713fb22451199c7b90d3ae9eef20418e8
ronarceo <ronaldmarceo@gmail.com>
2021-10-09 22:31:00 -0500

    made apiRoutes.js in routes folder, added totalDuration to tallyExercises function in workout.jsupdated server.js to use apiRoutes,

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({}).then(workouts => {
+        res.json(workouts);
+    })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 app.use(require("./routes/viewRoutes.js"));
+app.use(require("./routes/apiRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
