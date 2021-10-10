ac252246856c39cb1bbd1e9b53fc2b27be2baa2b
ronarceo <ronaldmarceo@gmail.com>
2021-10-09 22:55:04 -0500

    added route to get range of workouts in apiRoutes.js, removed extra closing div in index.html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,4 +10,12 @@ router.get("/api/workouts", (req, res) => {
         });
 })
 
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).then((workouts) => {
+        res.json(workouts);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 module.exports = router;
