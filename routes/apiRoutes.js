a3f18780886e29fd3633f2e440df0e7c929b9f68
ronarceo <ronaldmarceo@gmail.com>
2021-10-10 19:48:00 -0500

    added limit and sort to api/workouts/range route in apiRoutes.js to only get last seven workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,7 +19,8 @@ router.get("/api/workouts", (req, res) => {
 })
 
 router.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({}).then((workouts) => {
+    db.Workout.find({}).limit(7).sort({ day: -1})
+    .then((workouts) => {
         workouts.forEach(workouts => {
             let totalDuration = 0;
             workouts.exercises.forEach(e => {
@@ -28,7 +29,7 @@ router.get("/api/workouts/range", (req, res) => {
             workouts.totalDuration = totalDuration;
 
         });
-        res.json(workouts);
+        res.json(workouts.reverse());
     }).catch(err => {
         res.status(400).json(err);
     });
